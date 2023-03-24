e8910a0d689b8c50a7a680745b8a93943126c93f
Marclo Vargas Pavez <marcelo.vargas.p@usach.cl>
2023-03-24 18:01:19 -0300

    Creación proyecto en spring initializr

---
+++ evaluacion1/evaluacion1/src/test/java/com/tingeso/evaluacion1/Evaluacion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tingeso.evaluacion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
